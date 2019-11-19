b30c1a9d7e6f9f499a0c3c726e58ab7efed551ac
Emma Bowen <emloubow@gmail.com>
2019-11-19 14:11:14 +0000

    edit and delete flats done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     devise_for :users
   root to: 'pages#home'
-  resources :flats, only: [:index, :show, :new, :create] do
+  resources :flats do
     resources :bookings, only: [:index, :show, :new, :create]
   end
 end
