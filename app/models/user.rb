3d15f03d234e789dc5714f79ca0732d580735394
Doug Noble <douglasjmnoble@googlemail.com>
2019-11-19 15:13:46 +0000

    cloudinary_implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
   has_many :flats, dependent: :destroy
   has_many :bookings, dependent: :destroy
 end
