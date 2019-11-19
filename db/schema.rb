47a4d4fc336b9bf7903b9a233bb19bb7ef080302
Emma Bowen <emloubow@gmail.com>
2019-11-19 16:57:35 +0000

    fixed cloudinary issues

--- db/schema.rb
+++ db/schema.rb
@@ -86,10 +86,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_144517) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-=======
->>>>>>> master
   add_foreign_key "bookings", "flats"
   add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
