81eb2b61fe819918931d714d2d0b978083e27f1f
Joe Olley <joeolley@hotmail.co.uk>
2019-11-19 13:47:34 +0000

    bookings controller added with new and create actions and booking form partial on flat show page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+  validates :to_date, presence: true
+  validates :from_date, presence: true
 end
---
+++ test/controllers/bookings_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class BookingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get bookings_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get bookings_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get bookings_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get bookings_create_url
+    assert_response :success
+  end
+
+end
