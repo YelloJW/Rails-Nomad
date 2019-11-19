6aff70c327cc27901b42b8cb51e38956df3619a9
Emma Bowen <emloubow@gmail.com>
2019-11-19 12:49:13 +0000

    simple form for adding a new flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,11 @@
 class FlatsController < ApplicationController
-  before_action :find_flat, only: [:show, :create]
+  before_action :find_flat, only: [:show]
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @search = params[:search]
     if @search.nil?
-      @flats = Flats.all
+      @flats = Flat.all
     else
       @flats = Flat.where('address ILIKE ?', "%#{@search}%")
     end
@@ -15,14 +15,23 @@ class FlatsController < ApplicationController
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
   end
 
   private
 
   def flat_params
+    params.require(:flat).permit(:name, :address, :price_per_night, :electricity, :drinking_water, :sleeps, :description, :flat_type)
   end
 
   def find_flat
