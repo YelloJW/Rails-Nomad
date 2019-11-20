e7479415e200b6dc292bb7c0ce1c9b8e0cc14989
freddie warren <fredswow@live.co.uk>
2019-11-20 12:36:38 +0000

    when needed to login in for example a booking rquest you are now returned to the booking page to continue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  after_action :store_action
+
+  def store_action
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?)
+      store_location_for(:user, request.fullpath)
+    end
+  end
 end
