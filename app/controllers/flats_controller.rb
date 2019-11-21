5785b005a3d7b0a51e8178a93fc760680f5000d3
Emma Bowen <emloubow@gmail.com>
2019-11-21 11:14:14 +0000

    refactored the user show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -13,7 +13,7 @@ class FlatsController < ApplicationController
       {
         lat: flat.latitude,
         lng: flat.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: {flat: flat})
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat })
       }
     end
   end
