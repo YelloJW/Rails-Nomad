d8ae6baab460928bf7b2c762ed191c479ff57821
Joe Olley <joeolley@hotmail.co.uk>
2019-11-21 08:30:40 +0000

    user routes refactored and link updated in navbar - path now user_path(current_user)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
+  resources :users, only: [:show]
   resources :flats do
     resources :bookings, only: [:new, :create]
   end
